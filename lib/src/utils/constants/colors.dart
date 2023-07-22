4dfc32a6b789efd402ac4944a48ee195a99290ad
specOper99 <mohammadnawfal197@gmail.com>
2023-07-22 18:33:58 +0300

    create colors file

---
+++ lib/src/utils/constants/colors.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+const primaryColorLight = Colors.white;
+const primaryColorDark = Colors.black;
+
+const secondaryColorLight = Colors.black;
+const secondaryColorDark = Colors.white;
+
+const borderColor = Colors.grey;
+
+const voteColorVeryDisagree = const Color(0xffE80C14);
+const voteColorDisagree = const Color(0xffFF6060);
+const voteColorNeutral = const Color(0xffF5B60F);
+const voteColorAgree = const Color(0xff579BFC);
+const voteColorVeryAgree = const Color(0xff0753D9);
+
+const voteColorsList = [
+  voteColorVeryDisagree,
+  voteColorDisagree,
+  voteColorNeutral,
+  voteColorAgree,
+  voteColorVeryAgree
+];
